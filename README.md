b0b35132d9c82c929fac6c9efacb96289bbb982f
jcenturion <javier.centurion@auth0.com>
2016-02-19 16:20:22 -0300

    Refactoring code

---
+++ README.md
@@ -0,0 +1,76 @@
+# Auth0 - Logs to Azure
+
+This extension will take all of your Auth0 logs and export them to Azure Blob Storage.
+
+## Configure Azure Blob Storage
+
+First you'll need to [create an Azure Storage account](https://portal.azure.com/#create/Microsoft.StorageAccount.1.0.0).
+
+## Configure Webtask
+
+If you haven't configured Webtask on your machine run this first:
+
+```
+npm i -g wt-cli
+wt init
+```
+
+> Requires at least node 0.10.40 - if you're running multiple version of node make sure to load the right version, e.g. "nvm use 0.10.40"
+
+## Deploy to Webtask.io
+
+To run it on a schedule (run every 5 minutes for example):
+
+```
+wt cron schedule \
+    --name auth0-logs-to-azure \
+    --secret AUTH0_DOMAIN="YOUR_AUTH0_DOMAIN" \
+    --secret AUTH0_GLOBAL_CLIENT_ID="YOUR_AUTH0_GLOBAL_CLIENT_ID" \
+    --secret AUTH0_GLOBAL_CLIENT_SECRET="YOUR_AUTH0_GLOBAL_CLIENT_SECRET" \
+    --secret STORAGE_ACCOUNT_NAME="YOUR_STORAGE_ACCOUNT_NAME" \
+    --secret STORAGE_ACCOUNT_KEY="YOUR_STORAGE_ACCOUNT_KEY" \
+    --secret STORAGE_CONTAINER_NAME="YOUR_STORAGE_CONTAINER_NAME" \
+    --json \
+    "*/5 * * * *" \
+    https://raw.githubusercontent.com/auth0/auth0-logs-to-azure/master/index.js
+```
+
+> You can get your Global Client Id/Secret here: https://auth0.com/docs/api/v1
+
+## Usage
+
+Use any Storage Explorer to access your logs. Each record will be saved in the following structure:
+
+```
+https://{YOUR_STORAGE_ACCOUNT_NAME}.blob.core.windows.net/{YOUR_STORAGE_CONTAINER_NAME}/YYYY/MM/DD/HH/{LOG_ID}.json
+```
+
+This will allow you to process the logs with Stream Analytics for example (which in turn can export them to Power BI).
+
+## Issue Reporting
+
+If you have found a bug or if you have a feature request, please report them at this repository issues section. Please do not report security vulnerabilities on the public GitHub issue tracker. The [Responsible Disclosure Program](https://auth0.com/whitehat) details the procedure for disclosing security issues.
+
+## Author
+
+[Auth0](auth0.com)
+
+## What is Auth0?
+
+Auth0 helps you to:
+
+* Add authentication with [multiple authentication sources](https://docs.auth0.com/identityproviders), either social like **Google, Facebook, Microsoft Account, LinkedIn, GitHub, Twitter, Box, Salesforce, amont others**, or enterprise identity systems like **Windows Azure AD, Google Apps, Active Directory, ADFS or any SAML Identity Provider**.
+* Add authentication through more traditional **[username/password databases](https://docs.auth0.com/mysql-connection-tutorial)**.
+* Add support for **[linking different user accounts](https://docs.auth0.com/link-accounts)** with the same user.
+* Support for generating signed [Json Web Tokens](https://docs.auth0.com/jwt) to call your APIs and **flow the user identity** securely.
+* Analytics of how, when and where users are logging in.
+* Pull data from other sources and add it to the user profile, through [JavaScript rules](https://docs.auth0.com/rules).
+
+## Create a free Auth0 Account
+
+1. Go to [Auth0](https://auth0.com) and click Sign Up.
+2. Use Google, GitHub or Microsoft Account to login.
+
+## License
+
+This project is licensed under the MIT license. See the [LICENSE](LICENSE) file for more info.
---
+++ index.js
@@ -0,0 +1,293 @@
+"use latest";
+
+const Auth0     = require('auth0@0.8.2');
+const async     = require('async');
+const moment    = require('moment');
+const azure     = require('azure-storage');
+const useragent = require('useragent');
+
+const getCheckpointId = (history) => {
+  if (history && history.length > 0) {
+    console.log('Trying to get last checkpointId from previous run.');
+
+    for (let i = 0; i < history.length; i++) {
+      console.log (`Run: ${history[i].started_at} - ${history[i].type}`);
+
+      if (history[i].statusCode === 200 && history[i].body){
+        let result = JSON.parse(history[i].body);
+        if (result && result.checkpointId) {
+
+          console.log (`This is the last one we want to continue from: ${result.checkpointId}`);
+          return result.checkpointId;
+        }
+      }
+    };
+  }
+}
+
+module.exports = (ctx, done) => {
+  let required_settings = ['AUTH0_DOMAIN', 'AUTH0_GLOBAL_CLIENT_ID', 'AUTH0_GLOBAL_CLIENT_SECRET', 'STORAGE_ACCOUNT_NAME', 'STORAGE_ACCOUNT_KEY', 'STORAGE_CONTAINER_NAME'];
+  let missing_settings  = required_settings.filter((setting) => !ctx.data[setting]);
+  if (missing_settings.length) {
+    return done({ message: 'Missing settings: ' + missing_settings.join(', ') });
+  }
+
+  // If this is a scheduled task, we'll get the last log checkpoint from the previous run and continue from there.
+  let startCheckpointId = getCheckpointId(ctx.body && ctx.body.results);
+
+  // Initialize both clients.
+  const auth0 = new Auth0({
+     domain: ctx.data.AUTH0_DOMAIN,
+     clientID: ctx.data.AUTH0_GLOBAL_CLIENT_ID,
+     clientSecret: ctx.data.AUTH0_GLOBAL_CLIENT_SECRET
+  });
+  const blobService = azure.createBlobService(ctx.data.STORAGE_ACCOUNT_NAME, ctx.data.STORAGE_ACCOUNT_KEY);
+
+  // Start the process.
+  async.waterfall([
+    (callback) => {
+      auth0.getAccessToken((err) => {
+        if (err) {
+          console.log('Error authenticating:', err);
+        }
+        return callback(err);
+      });
+    },
+    (callback) => {
+      try {
+        blobService.createContainerIfNotExists(ctx.data.STORAGE_CONTAINER_NAME, (err) => callback(err));
+      } catch (e) {
+        return callback(e);
+      }
+    },
+    (callback) => {
+      const getLogs = (context) => {
+        console.log(`Downloading logs from: ${context.checkpointId || 'Start'}.`);
+
+        context.logs = context.logs || [];
+        auth0.getLogs({ take: 200, from: context.checkpointId }, (err, logs) => {
+          if (err) {
+            return callback(err);
+          }
+
+          if (logs && logs.length) {
+            logs.forEach((l) => context.logs.push(l));
+            context.checkpointId = context.logs[context.logs.length - 1]._id;
+            return setImmediate(() => getLogs(context));
+          }
+
+          console.log(`Total logs: ${context.logs.length}.`);
+          return callback(null, context);
+        });
+      };
+
+      getLogs({ checkpointId: startCheckpointId });
+    },
+    (context, callback) => {
+      context.logs = context.logs.map((record) => {
+        let level = 0;
+        record.type_code = record.type;
+        if (logTypes[record.type]) {
+          level = logTypes[record.type].level;
+          record.type = logTypes[record.type].event;
+        }
+
+        let agent = useragent.parse(record.user_agent);
+        record.os = agent.os.toString();
+        record.os_version = agent.os.toVersion();
+        record.device = agent.device.toString();
+        record.device_version = agent.device.toVersion();
+        return record;
+      });
+      callback(null, context);
+    },
+    (context, callback) => {
+      console.log('Uploading blobs...');
+
+      async.eachLimit(context.logs, 5, (log, cb) => {
+        const date = moment(log.date);
+        const url = `${date.format('YYYY/MM/DD')}/${date.format('HH')}/${log._id}.json`;
+        console.log(`Uploading ${url}.`);
+
+        blobService.createBlockBlobFromText(ctx.data.STORAGE_CONTAINER_NAME, url, JSON.stringify(log), cb);
+      }, (err) => {
+        if (err) {
+          return callback(err);
+        }
+
+        console.log('Upload complete.');
+        return callback(null, context);
+      });
+    }
+  ], function (err, context) {
+    if (err) {
+      console.log('Job failed.')
+      return done({ error: err }, {
+        checkpointId: startCheckpointId
+      });
+    }
+
+    console.log('Job complete.');
+    return done(null, {
+      checkpointId: context.checkpointId,
+      totalLogsProcessed: context.logs.length
+    });
+  });
+};
+
+const logTypes = {
+  's': {
+    event: 'Success Login',
+    level: 1 // Info
+  },
+  'seacft': {
+    event: 'Success Exchange',
+    level: 1 // Info
+  },
+  'feacft': {
+    event: 'Failed Exchange',
+    level: 3 // Error
+  },
+  'f': {
+    event: 'Failed Login',
+    level: 3 // Error
+  },
+  'w': {
+    event: 'Warnings During Login',
+    level: 2 // Warning
+  },
+  'du': {
+    event: 'Deleted User',
+    level: 1 // Info
+  },
+  'fu': {
+    event: 'Failed Login (invalid email/username)',
+    level: 3 // Error
+  },
+  'fp': {
+    event: 'Failed Login (wrong password)',
+    level: 3 // Error
+  },
+  'fc': {
+    event: 'Failed by Connector',
+    level: 3 // Error
+  },
+  'fco': {
+    event: 'Failed by CORS',
+    level: 3 // Error
+  },
+  'con': {
+    event: 'Connector Online',
+    level: 1 // Info
+  },
+  'coff': {
+    event: 'Connector Offline',
+    level: 3 // Error
+  },
+  'fcpro': {
+    event: 'Failed Connector Provisioning',
+    level: 4 // Critical
+  },
+  'ss': {
+    event: 'Success Signup',
+    level: 1 // Info
+  },
+  'fs': {
+    event: 'Failed Signup',
+    level: 3 // Error
+  },
+  'cs': {
+    event: 'Code Sent',
+    level: 0 // Debug
+  },
+  'cls': {
+    event: 'Code/Link Sent',
+    level: 0 // Debug
+  },
+  'sv': {
+    event: 'Success Verification Email',
+    level: 0 // Debug
+  },
+  'fv': {
+    event: 'Failed Verification Email',
+    level: 0 // Debug
+  },
+  'scp': {
+    event: 'Success Change Password',
+    level: 1 // Info
+  },
+  'fcp': {
+    event: 'Failed Change Password',
+    level: 3 // Error
+  },
+  'sce': {
+    event: 'Success Change Email',
+    level: 1 // Info
+  },
+  'fce': {
+    event: 'Failed Change Email',
+    level: 3 // Error
+  },
+  'scu': {
+    event: 'Success Change Username',
+    level: 1 // Info
+  },
+  'fcu': {
+    event: 'Failed Change Username',
+    level: 3 // Error
+  },
+  'scpn': {
+    event: 'Success Change Phone Number',
+    level: 1 // Info
+  },
+  'fcpn': {
+    event: 'Failed Change Phone Number',
+    level: 3 // Error
+  },
+  'svr': {
+    event: 'Success Verification Email Request',
+    level: 0 // Debug
+  },
+  'fvr': {
+    event: 'Failed Verification Email Request',
+    level: 3 // Error
+  },
+  'scpr': {
+    event: 'Success Change Password Request',
+    level: 0 // Debug
+  },
+  'fcpr': {
+    event: 'Failed Change Password Request',
+    level: 3 // Error
+  },
+  'fn': {
+    event: 'Failed Sending Notification',
+    level: 3 // Error
+  },
+  'sapi': {
+    event: 'API Operation'
+  },
+  'fapi': {
+    event: 'Failed API Operation'
+  },
+  'limit_wc': {
+    event: 'Blocked Account',
+    level: 4 // Critical
+  },
+  'limit_ui': {
+    event: 'Too Many Calls to /userinfo',
+    level: 4 // Critical
+  },
+  'api_limit': {
+    event: 'Rate Limit On API',
+    level: 4 // Critical
+  },
+  'sdu': {
+    event: 'Successful User Deletion',
+    level: 1 // Info
+  },
+  'fdu': {
+    event: 'Failed User Deletion',
+    level: 3 // Error
+  }
+};
